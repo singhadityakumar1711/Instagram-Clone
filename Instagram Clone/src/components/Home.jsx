67cae33bb7350537e0a0fb709d5b5f5804ab5016
Aditya Kumar Singh <singhadityakumar1711@gmail.com>
2024-03-03 17:53:15 +0530

    03-03-2024 17:53

--- Instagram Clone/src/components/Home.jsx
+++ Instagram Clone/src/components/Home.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
-
+import React from "react";
+import "./Home.css";
+import TextField from "@mui/material/TextField";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="card">
+        <div className="card-header">
+          <div className="profile-pic">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="p1"
+            />
+          </div>
+          <h5 className="card-author">r.y.u_k.e.n.d.o</h5>
+        </div>
+        <div className="card-img">
+          <img
+            src="https://images.unsplash.com/photo-1641202905211-1c82ff51f6e3?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="card-icons">
+          <span className="material-symbols-outlined">favorite</span>
+          <span className="material-symbols-outlined">tooltip</span>
+          <span className="material-symbols-outlined">send</span>
+        </div>
+        <div className="card-content">
+          <p className="likes">1 Like</p>
+          <span className="card-author">r.y.u_k.e.n.d.o</span>
+          <span className="caption">Mercedes Maybach....</span>
+        </div>
+        <div className="card-comment">
+          <TextField
+            id="standard-multiline-flexible"
+            label="Add a comment"
+            multiline
+            maxRows={4}
+            variant="standard"
+            name="email"
+            fullWidth
+            className="card-input"
+            sx={{ mt: 0.5 }}
+          />
+          <button className="post-button">POST</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-header">
+          <div className="profile-pic">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="p1"
+            />
+          </div>
+          <h5 className="card-author">r.y.u_k.e.n.d.o</h5>
+        </div>
+        <div className="card-img">
+          <img
+            src="https://images.unsplash.com/photo-1641202905211-1c82ff51f6e3?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="card-icons">
+          <span className="material-symbols-outlined">favorite</span>
+          <span className="material-symbols-outlined">tooltip</span>
+          <span className="material-symbols-outlined">send</span>
+        </div>
+        <div className="card-content">
+          <p className="likes">1 Like</p>
+          <span className="card-author">r.y.u_k.e.n.d.o</span>
+          <span className="caption">Mercedes Maybach....</span>
+        </div>
+        <div className="card-comment">
+          <TextField
+            id="standard-multiline-flexible"
+            label="Add a comment"
+            multiline
+            maxRows={4}
+            variant="standard"
+            name="email"
+            fullWidth
+            className="card-input"
+            sx={{ mt: 0.5 }}
+          />
+          <button className="post-button">POST</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- Instagram Clone/src/components/Profile.jsx
+++ Instagram Clone/src/components/Profile.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import "./Profile.css";
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="profile">
+      <div className="profile-header">
+        <div className="profile-pic-1">
+          <img
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="profile-details">
+          <div className="detail-1">
+            <p>r.y.u_k.e.n.d.o</p>
+            <button className="profile-btn">Edit profile</button>
+            <button className="profile-btn">View archive</button>
+          </div>
+          <div className="detail-2">
+            <span>31 posts</span>
+            <span>104 followers</span>
+            <span>79 following</span>
+          </div>
+          <div className="profile-bio">
+            <p>Aditya Kumar Singh</p>
+            <p className="bio">
+              NITDGP'24 ME <br />
+              Photography♥️ <br />
+              🎧Music Addicted🎶 <br />
+              🏏Cricket🥎|🏸 Badminton <br />
+              🎮Gamer🎮 <br />
+              📈Trading📈
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
